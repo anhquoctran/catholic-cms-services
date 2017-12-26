42d8a2c02051783d5f5dfae621b1b3612f8f8f60
Anh Quoc Tran <quoc.trananh@students.hueic.edu.vn>
2017-12-26 10:15:16 +0700

    add more constants

--- config/app_constants.php
+++ config/app_constants.php
@@ -10,3 +10,35 @@ if (!defined('DEFAULT_PAGINATION_PER_PAGE')) {
 if (!defined('DATE_TIME_FORMAT')) {
     define('DATE_TIME_FORMAT', 'Y-m-d H:i:s');
 }
+
+if(!defined('CHARGE_NEW')) {
+    define('CHARGE_NEW', 0);
+}
+
+if(!defined('CHARGE_UPDATE')) {
+    define('CHARGE_UPDATE', 1);
+}
+
+if(!defined('SORT_ASC')) {
+    define('SORT_ASC', 0);
+}
+
+if(!defined('SORT_DESC')) {
+    define('SORT_DESC', 1);
+}
+
+if(!defined('SORT_NONE')) {
+    define('SORT_NONE', 2);
+}
+
+if (!defined('SEARCH_CONTRIBUTE_ALL')) {
+    define('SEARCH_ALL', 0);
+}
+
+if (!defined('SEARCH_CONTRIBUTE_NEW')) {
+    define('SEARCH_CONTRIBUTE_NEW', 1);
+}
+
+if (!defined('SEARCH_CONTRIBUTE_NEW')) {
+    define('SEARCH_CONTRIBUTE_UPDATE', 2);
+}
