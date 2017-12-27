c662d8387b30648d1c1ff443af0a8ed93753fffc
Anh Quoc Tran <quoc.trananh@students.hueic.edu.vn>
2017-12-27 20:45:52 +0700

    add district feature

--- routes/web.php
+++ routes/web.php
@@ -11,30 +11,49 @@
 |
 */
 
-$app->get('/', function () use ($app) {
-    $array = ["message" => "Welcome to Sacred Heart Monastery REST Service!",  "success" => true, "code" => 200, "time" => \Carbon\Carbon::now()];
-    return response()->json($array);
-});
-
-/**
- * Login Route
- */
-$app->post('auth/login', 'AuthController@postLogin');
-
-$app->group(['middleware' => 'auth'], function ($app) {
+$app->group(['prefix' => 'v1'], function() use($app) {
+    $app->get('/', function () use ($app) {
+        $array = ["message" => "Welcome to Sacred Heart Monastery REST Service!",  "success" => true, "code" => 200, "time" => \Carbon\Carbon::now()];
+        return response()->json($array);
+    });
 
     /**
-     * Auth Route
+     * Login Route
      */
-    $app->post('auth/logout', 'AuthController@postLogout');
-    $app->put('auth/display_name', 'AuthController@putDisplayName');
-    $app->put('auth/password', 'AuthController@putPassword');
-    $app->post('auth/latest', 'AuthController@getLatest');
-    $app->post('auth/history', 'AuthController@getHistory');
+    $app->post('auth/login', 'AuthController@postLogin');
 
-    /**
-     * Diocese Route
-     */
-    $app->post('diocese/fetch_all', 'DioceseController@listDiocese');
+    $app->group(['middleware' => 'auth'], function () use($app) {
+
+        /**
+         * Auth Route
+         */
+        $app->group(['prefix' => 'auth'], function() use($app) {
+            $app->post('logout', 'AuthController@postLogout');
+            $app->put('display_name', 'AuthController@putDisplayName');
+            $app->put('password', 'AuthController@putPassword');
+            $app->post('latest', 'AuthController@getLatest');
+            $app->post('history', 'AuthController@getHistory');
+        });
+
+        /**
+         * Diocese Route
+         */
+        $app->post('diocese/fetch_all', 'DioceseController@listDiocese');
+
+        /**
+         * Province route
+         */
+        $app->group(['prefix' => 'provinces'], function() use($app) {
+            $app->get('fetch_all', 'ProvinceController@getListProvince');
+        });
+
+        /**
+         * District route
+         */
+        $app->group(['prefix' => 'districts'], function() use($app) {
+            $app->get('fetch_all', 'DistrictController@getListDistrict');
+            $app->get('get_by_province', 'DistrictController@getByProvince');
+            $app->get('single', 'DistrictController@getSingleDistrict');
+        });
+    });
 });
-
